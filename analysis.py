74b59978a3ab28bc6c5d9a959009e35d9604dc5e
danmuth <CKdjlor4>
2017-03-29 17:59:56 +1100

    Remove Anais's code (since incomplete)

--- analysis.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-data_tmp=pd.read_csv('classifications.csv',delimiter=',', index_col=False)
-names_df=pd.DataFrame(data=data_tmp['subject_id'].unique(),columns=['subject_id'])
-print len(data_tmp)
-df=pd.merge(data_tmp,names_df,on='subject_id')
-
-# print df
